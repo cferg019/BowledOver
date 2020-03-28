f2a3c7d791a2ada301b515abcb5103e84315fb86
Casey Ferguson <cferg019@gmail.com>
2020-03-28 15:12:21 -0400

    Added client side routing with framework for private routes

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,49 +1,46 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch, Redirect } from "react-router-dom";
+
 import './App.css';
 import Jumbotron from './components/Jumbotron'
 import Home from './components/Home'
 import Login from './components/Login'
+import Signup from './components/Signup'
+
 import Session from './components/Session'
 import Game from './components/Game'
 import Frame from './components/Frame'
 import logo from './logo.svg'
 
-
-
 class App extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      fakeAPIData: ''
-    }
-    fetch(`/api/user`)
-      .then(response => response.text())
-      .then(state => this.setState({ fakeAPIData: state }));
+  state = {
+    isAuthenticated: true
   }
   render() {
     return (
-      // <Jumbotron />
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload!
-        </p>
-          <p>
-            {this.state.fakeAPIData}
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-        </a>
-        </header>
-      </div>
-    );
+      <Router>
+        <div>
+        <Switch>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/signup">
+            <Signup />
+          </Route>
+          <PrivateRoute path="/" component={Home} isAuthenticated={this.state.isAuthenticated}/>
+        </Switch>
+        </div>
+      </Router>
+    )
   }
 }
+
+const PrivateRoute = ({ component: Component, isAuthenticated, ...rest }) => (
+  <Route {...rest} render={(props) => (
+    isAuthenticated === true
+      ? <Component {...props} />
+      : <Redirect to='/login' />
+  )} />
+)
 
 export default App;
--- react-app/src/index.js
+++ react-app/src/index.js
@@ -9,7 +9,7 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Login />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
