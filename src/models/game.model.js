205e10a14dbc20c6e74af4b864f58f5db1bf451c
Casey Ferguson <cferg019@gmail.com>
2020-04-01 20:23:04 -0400

    Added total score logic

--- src/models/game.model.js
+++ src/models/game.model.js
@@ -1,6 +1,7 @@
 const { Schema, model } = require('mongoose')
 const { mongooseSchema: FrameSchema, joiSchema: frameJoiSchema} = require('./frame.model')
 const joi = require('joi')
+const { getTotalScore } = require('../services/bowling-service')
 
 const joiSchema = joi.object().keys({
     _id: joi.string().length(24),
@@ -16,6 +17,13 @@ const gameSchema = new Schema({
     players: [{ type: Schema.Types.ObjectId }],
     frames: [FrameSchema]
 })
+
+gameSchema.virtual('totalScore').get(function() {
+    console.log('got to the virtual', getTotalScore(this.frames))
+    return getTotalScore(this.frames)
+})
+
+gameSchema.set('toJSON', {virtuals: true})
 
 module.exports = {
     mongooseSchema: gameSchema,
