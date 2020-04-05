458877b3ad2597b6f6b5b5d7fad9ab0d59799e3b
Casey Ferguson <cferg019@gmail.com>
2020-04-05 18:09:40 -0400

    Frame entry logic is working (except 10th frame)

--- react-app/src/components/Frames.js
+++ react-app/src/components/Frames.js
@@ -1,24 +1,110 @@
 import React, { Component } from 'react'
 import './Frames.css'
 
-const frames = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-const frameThrows = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+const frames = [...Array(10).keys()].map(key => key + 1) // 1 - 10
+const frameThrowSections = [...Array(21).keys()] // 0 - 20
 
 class Frames extends Component {
     state = {
         selected: 0,
-        pinsDown: 0,
-        currentThrow: 1
+        currentThrow: 1,
+        frameThrows: [...Array(21).keys()].map(() => ({
+            disabled: true,
+            displayValue: '',
+            value: ''
+        })),
+        buttons: [...Array(11).keys()].map(key => ({
+            disabled: false,
+            value: key,
+            displayValue: (key === 10) ? 'X' : key
+        }))
     }
 
     selectScore(index) {
-        console.log('clicked')
-        this.setState({ selected: index })
+        console.log('clicked', index)
+        if (!this.state.frameThrows[index].disabled) {
+            const prevButtonValue = (this.state.frameThrows[index - 1]) ? this.state.frameThrows[index - 1].value : null
+            let newButtons = this.getButtonsForThrow(prevButtonValue, index)
+            this.setState({ selected: index, buttons: newButtons })
+        }
     }
 
-    handleClick = (button) => {
+    handleScoreButtonClicked = (button) => {
         console.log('you got ', button)
-        this.setState({pinsDown: button})
+        let newSelected = this.state.selected
+        // make a copy
+        const frameThrowsCopy = [...this.state.frameThrows]
+        // Set the score for the button that clicked and the selected frame
+        frameThrowsCopy[this.state.selected] = {
+            disabled: false,
+            displayValue: button.displayValue,
+            value: button.value
+        }
+        // Make the next frame enabled in case you need to go back before complete
+        if (frameThrowsCopy[this.state.selected + 1]) {
+            frameThrowsCopy[this.state.selected + 1].disabled = false
+        }
+        // Increment the selected frame by 1 so it moves over after the score is input
+        newSelected++
+        // Handle if it's a strike (skip the next throw in frame)
+        if (button.displayValue === 'X') {
+            // Not the 10th frame
+            if (this.state.selected < 18) {
+                frameThrowsCopy[this.state.selected + 1] = {
+                    disabled: true,
+                    value: '',
+                    displayValue: ''
+                }
+                newSelected++;
+            }
+        }
+        if (this.state.selected % 2 === 0 && this.state.frameThrows[this.state.selected + 1]) {
+            this.state.frameThrows[this.state.selected + 1].value = ''
+            this.state.frameThrows[this.state.selected + 1].displayValue = ''
+            // If we're in the middle of entering, and the user goes back to the previous frames first throw, 
+            // and changes it, that wipes out the second throw, so we want to disable the first throw of the next frame until
+            // they enter the value for the second throw of the previous frame
+            if (this.state.frameThrows[this.state.selected + 2] && this.state.frameThrows[this.state.selected + 2].value === '') {
+                frameThrowsCopy[this.state.selected + 2].disabled = true
+            }
+        }
+        let newButtons = this.getButtonsForThrow(button.value, newSelected)
+
+        this.setState({ frameThrows: frameThrowsCopy, selected: newSelected, buttons: newButtons })
+    }
+
+    getButtonsForThrow(buttonValue, selected) {
+
+        console.log('button value', buttonValue, 'selected', selected)
+        const buttonsCopy = [...Array(11).keys()].map(key => ({
+            disabled: false,
+            value: key,
+            displayValue: (key === 10) ? 'X' : key
+        })) 
+
+        // Tenth frame
+        if (this.state.selected > 17) {
+            
+        }
+        // It's a strike, so return all possible values for the next throw
+        if (selected % 2 === 0) {
+            console.log('it should reset')
+            return buttonsCopy
+        } 
+        else if (buttonValue === 0) {
+            buttonsCopy[10].displayValue = '/'
+        }
+        else {
+            buttonsCopy.forEach((button, i) => {
+                if (i === (10 - buttonValue)) {
+                    button.displayValue = '/'
+                }
+                if (i > (10 - buttonValue)) {
+                    button.disabled = true
+                }
+            })
+        }
+        return buttonsCopy
     }
 
     render() {
@@ -28,18 +114,26 @@ class Frames extends Component {
                     <div className="table-responsive">
                         <table className="table table-bordered table-hover">
                             <tr>
-                                {frames.map(frame => (<th colspan="2">Frame {frame}</th>))}
+                                {frames.map(frame => {
+                                    const colspan = (frame === 10) ? '3' : '2'
+                                    return (<th colspan={colspan}>Frame {frame}</th>)
+                                })}
                             </tr>
                             <tr className="throwsRow">
-                                {frameThrows.map((frame, i) => {
-                                    const classes = this.state.selected === i ?
+                                {frameThrowSections.map((frame, i) => {
+                                    let classes = this.state.selected === i ?
                                         'throw selected' :
-                                        'throw'
-                                    return (<td onClick={() => this.selectScore(i)} className={classes}>{this.state.pinsDown}</td>)
+                                        'throw';
+                                    if (this.state.frameThrows[i].disabled)
+                                        classes += ' disabled'
+                                    return (<td onClick={() => this.selectScore(i)} className={classes}>{this.state.frameThrows[i].displayValue}</td>)
                                 })}
                             </tr>
                             <tr className="scoreRow">
-                                {frames.map(frame => (<td className="score" colspan="2">Score {frame}</td>))}
+                                {frames.map(frame => {
+                                    const colspan = (frame === 10) ? '3' : '2'
+                                    return (<td className="score" colspan={colspan}>Score {frame}</td>)
+                                })}
                             </tr>
 
                         </table>
@@ -47,28 +141,18 @@ class Frames extends Component {
                 </div>
                 <div className="row">
                     <div className="col btn-col">
-                        <button type="button" className="btn btn-lg btn-outline-secondary" value="0" 
-                            onClick={() => this.handleClick(0)}>0</button>
-                        <button type="button" className="btn btn-lg btn-outline-secondary" value="1"
-                            onClick={() => this.handleClick(1)}>1</button>
-                        <button type="button" className="btn btn-lg btn-outline-secondary" value="2"
-                            onClick={() => this.handleClick(2)}>2</button>
-                        <button type="button" className="btn btn-lg btn-outline-secondary" value="3"
-                            onClick={() => this.handleClick(3)}>3</button>
-                        <button type="button" className="btn btn-lg btn-outline-secondary" value="4"
-                            onClick={() => this.handleClick(4)}>4</button>
-                        <button type="button" className="btn btn-lg btn-outline-secondary" value="5"
-                            onClick={() => this.handleClick(5)}>5</button>
-                        <button type="button" className="btn btn-lg btn-outline-secondary" value="6"
-                            onClick={() => this.handleClick(6)}>6</button>
-                        <button type="button" className="btn btn-lg btn-outline-secondary" value="7"
-                            onClick={() => this.handleClick(7)}>7</button>
-                        <button type="button" className="btn btn-lg btn-outline-secondary" value="8"
-                            onClick={() => this.handleClick(8)}>8</button>
-                        <button type="button" className="btn btn-lg btn-outline-secondary" value="9"
-                            onClick={() => this.handleClick(9)}>9</button>
-                        <button type="button" className="btn btn-lg btn-outline-secondary" value="X"
-                            onClick={() => this.handleClick('X')}>X</button>
+                        {this.state.buttons
+                            .filter(button => !button.disabled)
+                            .map(button => (
+                            <button
+                                type="button"
+                                className="btn btn-lg btn-outline-secondary"
+                                value={button.displayValue}
+                                onClick={() => this.handleScoreButtonClicked(button)}
+                            >
+                                {button.displayValue}
+                            </button>
+                        ))}
                     </div>
                 </div>
             </div>
