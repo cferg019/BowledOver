9a920c35c1b5a983a4b94105ac7e08d06392ff2d
Casey Ferguson <cferg019@gmail.com>
2020-04-08 21:06:04 -0400

    dont build react on heroku

---
+++ react-app/build/precache-manifest.2d35d4212a748c3ba99b74b8625e2acf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5e0daf7a13832be71a634a84af3371d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bb36d30fcc2c6a9afe03",
+    "url": "/static/css/2.3fd47f61.chunk.css"
+  },
+  {
+    "revision": "c33f3fc9c94ec987c9d2",
+    "url": "/static/css/main.19af36cd.chunk.css"
+  },
+  {
+    "revision": "bb36d30fcc2c6a9afe03",
+    "url": "/static/js/2.7d7e5995.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.7d7e5995.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c33f3fc9c94ec987c9d2",
+    "url": "/static/js/main.0515e219.chunk.js"
+  },
+  {
+    "revision": "e09b3a133424f3eaaad2",
+    "url": "/static/js/runtime-main.d7a593f2.js"
+  }
+]);
