0634b9ec13ef30dc17a584ed4cf0eb09fa14d41a
Casey Ferguson <cferg019@gmail.com>
2020-04-10 21:50:12 -0400

    built react app

---
+++ react-app/build/precache-manifest.71f2a8669e7e0acaef4d46e7d49b07d2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a6eb76a27ef3e474e190d3ad63bb4f6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "13a62b193f7bf6d6a5d2",
+    "url": "/static/css/2.3fd47f61.chunk.css"
+  },
+  {
+    "revision": "9027e00ad05c61b22f87",
+    "url": "/static/css/main.8c4d6695.chunk.css"
+  },
+  {
+    "revision": "13a62b193f7bf6d6a5d2",
+    "url": "/static/js/2.e3d2bc6e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.e3d2bc6e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9027e00ad05c61b22f87",
+    "url": "/static/js/main.25846828.chunk.js"
+  },
+  {
+    "revision": "e09b3a133424f3eaaad2",
+    "url": "/static/js/runtime-main.d7a593f2.js"
+  },
+  {
+    "revision": "882c5d72bced8fe7691c11baec219d33",
+    "url": "/static/media/black.882c5d72.jpg"
+  }
+]);
